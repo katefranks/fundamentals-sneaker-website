ac3c21df5bb88e662c7ff9d4fe27ffb8090a4d3b
katefranks <kmfranks9@gmail.com>
2021-08-13 20:30:47 -0400

    updated styling & added js file

---
+++ main.js
@@ -0,0 +1,42 @@
+// console.log('Hello, World!');
+
+var age = 30;
+
+age = 31;
+
+// console.log(age);
+
+var message = "welcome to JS";
+
+
+console.log(message, message, message, message);
+
+
+var fifty = 50;
+var hundred = 100;
+var fiveHundred = 500;
+
+var sum = fifty + hundred + fiveHundred
+
+console.log(sum);
+
+var person = {
+  name : "jenny",
+  age : 28,
+
+}
+console.log(person);
+
+person.pet = 'tucker';
+
+console.log(person);
+
+console.log(typeof person);
+
+var students = ["Alejandra", "Jesse", "Jessica", "Tamar"];
+console.log(students[1]);
+
+students[1] = 'Kate';
+console.log(students[1]);
+
+console.log(students);
